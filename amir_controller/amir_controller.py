699655677f49e6bcaed2a66e1a8fed8e9bbecb10
MrCrow12 <109727888+MrCrow12@users.noreply.github.com>
2025-01-01 20:54:59 +0330

    Update amir_controller.py

--- amir_controller/amir_controller.py
+++ amir_controller/amir_controller.py
@@ -2,27 +2,27 @@ import os
 import requests
 import time
 
-
-try:
-    os.system("cls")
-    print("Check internet connection...")
-    x=requests.get("https://mrcrow.pythonanywhere.com/getCommend").json()
-    print("Check server...")
-    time.sleep(1)
-    os.system("cls")
-    path=os.path.dirname(os.path.abspath(__file__))
-    user=""
-    while not user:
-        user=input("Enter your user:")
-    with open(f"{path}\\user.txt" , 'w') as file:
-        file.write(user)
-    cmd=f'{path[:2]} & cd "{path}" & start /B pythonw controller.py'
-    os.system(command=cmd)
-    print("program is activated.")
-    time.sleep(1)
-except:
-    print("Network Error")
-    time.sleep(1)
+def run():
+    try:
+        os.system("cls")
+        print("Check internet connection...")
+        x=requests.get("https://mrcrow.pythonanywhere.com/getCommend").json()
+        print("Check server...")
+        time.sleep(1)
+        os.system("cls")
+        path=os.path.dirname(os.path.abspath(__file__))
+        user=""
+        while not user:
+            user=input("Enter your user:")
+        with open(f"{path}\\user.txt" , 'w') as file:
+            file.write(user)
+        cmd=f'{path[:2]} & cd "{path}" & start /B pythonw controller.py'
+        os.system(command=cmd)
+        print("program is activated.")
+        time.sleep(1)
+    except:
+        print("Network Error")
+        time.sleep(1)
 
 if __name__=="__main__":
     pass
